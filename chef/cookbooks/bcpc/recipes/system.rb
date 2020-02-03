e481b73b258b1d26cf53376ed0f4a7e3c3159ad1
David A. Desrosiers <setuid@gmail.com>
2020-02-03 16:58:09 -0500

    Quoting change to address Travils failures

--- chef/cookbooks/bcpc/recipes/system.rb
+++ chef/cookbooks/bcpc/recipes/system.rb
@@ -58,7 +58,7 @@ template '/etc/default/grub' do
   cmdline = []
   # This addresses the issue with controller vs. disk/device naming issues,
   # providing consistent, deterministic device naming conventions.
-  cmdline.push("nvme_core.multipath=0")
+  cmdline.push('nvme_core.multipath=0')
 
   io_scheduler = node['bcpc']['hardware']['io_scheduler']
   cmdline.push("elevator=#{io_scheduler}")
